160dfac828ccbbd1b25cea9775a30482c27a84e5
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-03-09 12:40:45 +0100

    3. create GlobalStyle.js

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-size: 100%;
+        overflow-x: hidden;
+        background: #fff;
+        font-family: 'Barlow', sans-serif;
+        overflow-x: hidden;
+    }
+
+    a {
+        text-decoration: none;
+        font-family: 'Barlow', sans-serif;
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+    }
+`
+export default GlobalStyle
