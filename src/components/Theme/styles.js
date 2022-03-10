92a2083b2711ea0968402fe1033bc5eda2792465
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-03-10 22:25:41 +0100

    18. create style.js

---
+++ src/components/Theme/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { theme } from '../Theme'
+
+export const StyleTemplate = styled.article`
+    width: 33%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.8em 0;
+
+    span {
+        letter-spacing: ${theme.letterSpacing};
+        color: ${theme.lightScundariFontColor};
+        margin: 0.6em 0 0.3em 0;
+    }
+    p {
+        font-size: 0.5em;
+        letter-spacing: 0.1em;
+        color: ${theme.lightPrimariFontColor};
+    }
+`
