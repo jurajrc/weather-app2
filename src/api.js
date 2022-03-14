695ecf798433ca6edc2283b64f30981cb0ab788a
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-03-14 20:28:40 +0100

    36. refac. BASE_URL api.js

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const API_KEY = "3c92a025d4a82657c8444d3c6bcda105"
 export const api_geo = (term) => `${BASE_URL}/geo/1.0/direct?q=${term}&limit=1&appid=${API_KEY}`
 
 // response detail weather by specified coords
-export const api_coord = (suradnice) => `https://api.openweathermap.org/data/2.5/onecall?lat=${suradnice.lat}&lon=${suradnice.lon}&exclude=minutely,hourly&appid=${API_KEY}&units=metric&lang=sk`
+export const api_coord = (suradnice) => `${BASE_URL}data/2.5/onecall?lat=${suradnice.lat}&lon=${suradnice.lon}&exclude=minutely,hourly&appid=${API_KEY}&units=metric&lang=sk`
 
 
 // use .env
