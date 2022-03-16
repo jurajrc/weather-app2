3b8a5429d48e68289bd17a32ce2c322b8296e34f
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-03-16 18:09:55 +0100

    40 add coords my towns

--- src/coordsTown.js
+++ src/coordsTown.js
@@ -1,21 +1,33 @@
 export const coordsTown = [
     {
         town: "Bratislava",
+        lat: 48.152,
+        lon: 17.109,
         temp: 35
     },{
         town: "Humenné",
+        lat: 48.935,
+        lon: 21.902,
         temp: 18
     },{
         town: "Koromľa",
+        lat: 48.716,
+        lon: 22.293,
         temp: 28
     },{
         town: "Košice",
+        lat: 48.717,
+        lon: 21.250,
         temp: 28
     },{
         town: "Michalovce",
+        lat: 48.751,
+        lon: 21.921,
         temp: 16
     },{
         town: "Sobrance",
+        lat: 48.745,
+        lon: 22.181,
         temp: 16
     }
 ]
